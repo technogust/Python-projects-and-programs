8a03930c77303181757469172937cc218851d29b
12micplots <40589427+12micplots@users.noreply.github.com>
2019-07-30 04:21:12 +0530

    NCERT evolution notes program plus a short quiz

---
+++ evolution chapter + quiz/Question.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 03:23:04 2019
+
+@author: vaibh
+"""
+
+class Question:
+    def __init__(self,prompt,answer):
+        self.prompt= prompt
+        self.answer=answer
---
+++ evolution chapter + quiz/evolution.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 30 00:00:58 2019
+
+@author: vaibh
+"""
+
+from Question import Question
+
+print("Origin of life")
+print("1.spores/Panspermia")
+print("2.spontaneous generation")
+print("3.chemical evolution")
+print("4. Exit / move to next section")
+
+while True:
+    choice = int(input("out of them which theory would you choose :"))
+    if choice == 1:
+        print("Panspermia’ is still a favourite idea for some astronomers.")
+        print("1.spores/Panspermia")
+        print("2.spontaneous generation")   
+        print("3.chemical evolution")
+        print("4. Exit / move to next section")
+
+    elif choice == 2:
+        print("For a long time it was also believed that life came out of decaying and rotting matter like straw, mud, etc. This was the theory of spontaneous generation. Louis Pasteur yeast experimentation demonstrated that life comes only from pre-existing life. Spontaneous generation theory was dismissed once and for all.")
+        print("1.spores/Panspermia")
+        print("2.spontaneous generation")   
+        print("3.chemical evolution")
+        print("4. Exit / move to next section")
+
+    elif choice == 3:
+        print("correct: In 1953, S.L. Miller, an American scientist created similar earth like conditions in laboratory and formation of organic molecules occured from inorganic molecules.")
+        print("1.spores/Panspermia")
+        print("2.spontaneous generation")   
+        print("3.chemical evolution")
+        print("4. Exit / move to next section")
+
+    elif choice == 4:
+        break
+
+print("Evolutions of life forms: a theory")
+print("1.Religious theories")
+print("2.Darwin theory (19th Century)")
+print("3.Alfred Wallace theory")
+print("4. Exit / move to next section")
+
+while True:
+    choice = int(input("which theory would you support. Enter your choice to know more about them."))
+    if choice == 1:
+        print("This theory has three connotations. One, that all living organisms (species or types) that we see today were created as such. Two, that the diversity was always the same since creation and will be the same in future also. Three, that earth is about 4000 years old. All these ideas were strongly challanged during 19th Century and common conclusion was found out that Earth is not that old.")
+        print("1.Religious theories")
+        print("2.Darwin theory (19th Century)")
+        print("3.Alfred Wallace theory")
+        print("4. Exit / move to next section")
+
+    elif choice == 2:
+        print("Charles Darwin concluded that existing living forms share similarities to varying degrees not only among themselves but also with life forms that existed millions of years ago. Many such life forms do not exist any more. There had been extinctions of different life forms in the years gone by just as new forms of life arose at different periods of history of earth. The fitness, according to Darwin, refers ultimately and only to reproductive fitness")
+        print("1.Religious theories")
+        print("2.Darwin theory (19th Century)")
+        print("3.Alfred Wallace theory")
+        print("4. Exit / move to next section")
+
+    elif choice == 3:
+        print("In due course of time, apparently new types of organisms are recognisable. All the existing life forms share similarities and share common ancestors. However, these ancestors were present at different periods in the history of earth")
+        print("1.Religious theories")
+        print("2.Darwin theory (19th Century)")
+        print("3.Alfred Wallace theory")
+        print("4. Exit / move to next section")
+
+    elif choice == 4:
+        break
+    
+print("WHAT ARE THE EVIDENCES FOR EVOLUTION?")
+print("1.paleontological evidence")
+print("2.homologous structures and divergent evolution")
+print("3.convergent evolution and analogous structures")
+print("4. Exit / move to next section")
+while True:
+    choice = int(input("which theory would you support. Enter your choice to know more about them."))
+    if choice == 1:
+        print("Fossils are remains of hard parts of life-forms found in rocks. Rocks form sediments and a cross-section of earth's crust indicates the arrangement of sediments one over the other during the long history of earth. Different-aged rock sediments contain fossils of different life-forms who probably died during the formation of the particular sediment. Some of them appear similar to modern organisms")
+        print("1.paleontological evidence")
+        print("2.homologous structures and divergent evolution")
+        print("3.convergent evolution and analogous structures")
+        print("4. Exit / move to next section")
+
+    elif choice == 2:
+        print("In animals when the same structure developed along different directions due to adaptations to different needs. This is divergent evolution and these structures are homologous. Homology indicates common ancestry.")
+        print("1.paleontological evidence")
+        print("2.homologous structures and divergent evolution")
+        print("3.convergent evolution and analogous structures")
+        print("4. Exit / move to next section")
+
+    elif choice == 3:
+        print("analogous structures are a result of convergent evolution -different structures evolving for the same function and hence having similarity. Other examples of analogy are the eye of the octopus and of mammals or the flippers of Penguins and Dolphins. One can say that it is the similar habitat that has resulted in selection of similar adaptive features in different groups of organisms but toward the same function: Sweet potato (root modification) and potato (stem modification) is another example for analogy.")
+        print("1.paleontological evidence")
+        print("2.homologous structures and divergent evolution")
+        print("3.convergent evolution and analogous structures")
+        print("4. Exit / move to next section")
+
+    elif choice == 4:
+        break
+    
+print("What is adaptive radiation? ")
+print("This process of evolution of different species in a given geographical area starting from a point and literally radiating to other areas of geography (habitats) is called adaptive radiation.")
+choice = int(input("enter 1 to continue"))
+if choice == 1:
+    print("Mechanism of Evolution")
+    print("Hugo deVries based on his work on evening primrose brought forth the idea of mutations – large difference arising suddenly in a population. He believed that it is mutation which causes evolution and not the minor variations (heritable) that Darwin talked about. Mutations are random and directionless while Darwinian variations are small and directional. Evolution for Darwin was gradual while deVries believed mutation caused speciation and hence called it saltation (single step large mutation).")
+
+choice = int(input("enter 1 to continue"))
+if choice == 1:
+    print("HARDY-WEINBERG PRINCIPLE")
+    print("This principle says that allele frequencies in a population are stable and is constant from generation to generation. The gene pool (total genes and their alleles in a population) remains a constant. This is called genetic equilibrium. Five factors are known to affect Hardy-Weinberg equilibrium. These are gene migration or gene flow, genetic drift, mutation, genetic recombination and natural selection. When migration of a section of population to another place and population occurs, gene frequencies change in the original as well as in the new population. New genes/alleles are added to the new population and these are lost from the old population. There would be a gene flow if this gene migration, happens multiple times. If the same change occurs by chance, it is called genetic drift. Sometimes the change in allele frequency is so different in the new sample of population that they become a different species. The original drifted population becomes founders and the effect is called founder effect.")
+
+choice = int(input("enter 1 to continue"))
+if choice == 1:
+    print("A BRIEF ACCOUNT OF EVOLUTION")
+    print("About 2000 million years ago (mya) the first cellular forms of life appeared on earth. The mechanism of how non-cellular aggregates of giant macromolecules could evolve into cells with membranous envelop is not known. Some of these cells had the ability to release O2. The reaction could have been similar to the light reaction in photosynthesis where water is split with the help of solar energy captured and channelised by appropriate light harvesting pigments. Slowly single-celled organisms became multi-cellular life forms. By the time of 500 mya, invertebrates were formed and active. Jawless fish probably evolved around 350 mya. Sea weeds and few plants existed probably around 320 mya. We are told that the first organisms that invaded land were plants. They were widespread on land when animals invaded land. Fish with stout and strong fins could move on land and go back to water. This was about 350 mya. In 1938, a fish caught in South Africa happened to be a Coelacanth which was thought to be extinct. These animals called lobefins evolved into the first amphibians that lived on both land and water. There are no specimens of these left with us. However, these were ancestors of modern day frogs and salamanders. The amphibians evolved into reptiles. They lay thickshelled eggs which do not dry up in sun unlike those of amphibians. Again we only see their modern day descendents, the turtles, tortoises and crocodiles. In the next 200 millions years or so, reptiles of different Representative evolutionary history of vertebrates through geological periods shapes and sizes dominated on earth. Giant ferns (pteridophytes) were present but they all fell to form coal deposits slowly. Some of these land reptiles went back into water to evolve into fish like reptiles probably 200 mya (e.g. Ichthyosaurs). The land reptiles were, of course, the dinosaurs. The biggest of them, i.e., Tyrannosaurus rex was about 20 feet in height and had huge fearsome dagger like teeth. About 65 mya, the dinosaurs suddenly disappeared from the earth. We do not know the true reason. Some say climatic changes killed them. Some say most of them evolved into birds. The truth may live in between. Small sized reptiles of that era still exist today. The first mammals were like shrews. Their fossils are small sized. Mammals were viviparous and protected their unborn young inside the mother’s body. Mammals were more intelligent in sensing and avoidingdanger at least. When reptiles came down mammals took over this earth. There were in South America mammals resembling horse, hippopotamus,bear, rabbit, etc. Due to continental drift, when South America joinedNorth America, these animals were overridden by North American fauna.Due to the same continental drift pouched mammals of Australia survived because of lack of competition from any other mammal.")
+
+choice = int(input("enter 1 to continue"))
+if choice == 1: 
+    print("ORIGIN AND EVOLUTION OF MAN")
+    print("About 15 mya, primates called Dryopithecus and Ramapithecus were existing. They were hairy and walked like gorillas and chimpanzees. Ramapithecus was more man-like while Dryopithecus was more ape-like. Few fossils of man-like bones have been discovered in Ethiopia and Tanzania (Figure 7.11). These revealed hominid features leading to the belief that about 3-4 mya, man-like primates walked in eastern Africa. They were probably not taller than 4 feet but walked up right. Two mya, Australopithecines probably lived in East African grasslands. Evidence shows they hunted with stone weapons but essentially ate fruit. Some of the bones among the bones discovered were different. This creature was called the first human-like being the hominid and was called Homo habilis. The brain capacities were between 650-800cc. They probably did not eat meat. Fossils discovered in Java in 1891 revealed the next stage, i.e., Homo erectus about 1.5 mya. Homo erectus had a large brain around 900cc. Homo erectus probably ate meat. The Neanderthal man with a brain size of 1400cc lived in near east and central Asia between 1,00,000-40,000 years back. They used hides to protect their body and buried their dead. Homo sapiens arose in Africa and moved across continents and developed into distinct races. During ice age between 75,000-10,000 years ago modern Homo sapiens arose.")
+    
+print("Quiz Time")
+question_prompts = [
+        "which is the most acceptable theory? \n(a)spores \n (b)random evolution \n (c)spontaneous generation \n (d)chemical evolution ",
+        "which theory is least accepted? \n(a)Religious theories \n (b)Darwin theory (19th Century) \n (c) Lamaar theory (19th Century) \n (d)Alfred Wallace theory",
+        "Which of THE following is not EVIDENCE FOR EVOLUTION? \n(a)paleontological evidence \n (b)rock salt \n (c)homologous structures \n (d)analogous structures ",
+        "when first life appeared? \n(a) 1 million years ago \n (b) 20 mya \n (c)3000 mya \n (d) 2000 mya ",
+        "when origin and evolution of man started? \n(a)15 mya \n (b)25 mya \n (c)35 mya \n (d) 45 mya "
+        ]
+
+questions = [
+        Question(question_prompts[0],"d"),
+        Question(question_prompts[1],"a"),
+        Question(question_prompts[2],"b"),
+        Question(question_prompts[3],"d"),
+        Question(question_prompts[4],"a")
+        
+        ]
+
+def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got " + str(score) + "/" + str(5) + " Correct")
+    
+run_test(questions)
+        
